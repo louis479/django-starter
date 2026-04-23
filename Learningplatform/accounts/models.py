0d0fd9b82d1ab2fa18e0d8a0d4f62c53fbfd6a9c
louis-ngugi <louisngugi858@gmail.com>
2026-04-24 00:43:37 +0300

    Created a Course model  Page

--- Learningplatform/accounts/models.py
+++ Learningplatform/accounts/models.py
@@ -2,3 +2,18 @@ from django.db import models
 
 # Create your models here.
 from django.contrib.auth.models import User
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Enrollment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} -> {self.course.title}"
